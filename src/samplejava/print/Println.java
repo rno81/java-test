384689c5b4110bbe3d15ff7472c5f6858b9b9566
rno81 <rno.tqm@gmail.com>
2014-02-12 15:24:10 +0100

    Println class added

---
+++ src/samplejava/print/Println.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package samplejava.print;
+
+/**
+ *
+ * @author promobile
+ */
+public class Println {
+
+    public static final void print(String s){
+        System.out.println(s);
+    }
+
+}
